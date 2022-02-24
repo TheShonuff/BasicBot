ada106e560def38f85eac5cbc0702f9047c4d129
TheShonuff <jirvine124@gmail.com>
2022-02-23 21:15:55 -0700

    heroku runtime flies

--- math_functions.py
+++ math_functions.py
@@ -1,5 +1,3 @@
-import math
-
 def rect_to_polar():
     a = input("Input resistor value: ")
     b = input("input capacitive reactance value: ")
@@ -13,3 +11,25 @@ def rect_to_polar():
     print(f'Z = {magnitude} \u2221 {phase_angle}')
 
 
+def convertPolar2(ctx):
+        
+        await ctx.send('Input resistor value: ')
+        message_response = await bot.wait_for('message')
+        a = (message_response.content)
+        
+        await ctx.send('Input capacitive reactance value: ')
+        message_response = await bot.wait_for('message')
+        b = (message_response.content)
+
+        # a = input("Input resistor value: ")
+        # b = input("input capacitive reactance value: ")
+
+        magnitude = math.sqrt((int(a)**2) + (int(b)**2))
+        print(magnitude)
+
+        phase_angle = math.degrees(math.atan(int(b)/int(a)) )
+        print(phase_angle)
+
+        # result = print(f'Z = {magnitude} \u2221 {phase_angle}')
+        
+        await ctx.send(f'z = {magnitude} \u2221 {phase_angle}')
